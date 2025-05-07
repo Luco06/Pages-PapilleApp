fdf160a4bcabd68e5ab5d27738e06012bd4a1285
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-05-07 12:15:48 +0200

    AJout d'une icon optionnelle a l'input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,14 +1,16 @@
+import { ReactElement } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { useTheme } from '../theme/themeContext';
 
 type InputProps = {
   label: string;
   value: string;
+  iconRight?: ReactElement;
   secureTextEntry?: boolean;
   onChangeText: (text: string) => void;
 };
 
-export default function Input({ label, value, onChangeText, secureTextEntry }: InputProps) {
+export default function Input({ label, value, onChangeText, secureTextEntry, iconRight }: InputProps) {
   const theme = useTheme();
 
   return (
@@ -16,18 +18,22 @@ export default function Input({ label, value, onChangeText, secureTextEntry }: I
       <Text style={[styles.label, { color: theme.colors.text, fontSize: theme.fontSize.small }]}>
         {label}
       </Text>
-      <TextInput
-        style={[
-          styles.input,
-          {
-            borderColor: theme.colors.primary,
-            color: theme.colors.text,
-          },
-        ]}
-        secureTextEntry={secureTextEntry}
-        onChangeText={onChangeText}
-        value={value}
-      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={[
+            styles.input,
+            {
+              borderColor: theme.colors.primary,
+              color: theme.colors.text,
+              paddingRight: iconRight ? 40 : 10, // pour éviter que le texte passe sous l'icône
+            },
+          ]}
+          secureTextEntry={secureTextEntry}
+          onChangeText={onChangeText}
+          value={value}
+        />
+        {iconRight && <View style={styles.iconRight}>{iconRight}</View>}
+      </View>
     </View>
   );
 }
@@ -40,10 +46,20 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     textAlign: 'left',
   },
+  inputContainer: {
+    position: 'relative',
+    justifyContent: 'center',
+  },
   input: {
     borderRadius: 20,
     borderWidth: 1,
     padding: 10,
     height: 50,
+    paddingLeft: 10,
+  },
+  iconRight: {
+    position: 'absolute',
+    right: 10,
+    top: 13, // ajuste selon le design
   },
 });
