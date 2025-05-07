92bae1d50436240c3847a786c18f4f04d9e98e30
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-05-07 12:14:52 +0200

    AJout de la mutation inscription dans la page d'inscription

--- app/Signin.tsx
+++ app/Signin.tsx
@@ -1,4 +1,6 @@
-import { Link } from "expo-router";
+import { useMutation } from "@apollo/client";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Link, useRouter } from "expo-router";
 import { useState } from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -6,14 +8,32 @@ import Button from "../components/Button";
 import Input from "../components/Input";
 import TextAuthPage from "../components/TextAuthPage";
 import Title from "../components/Title";
+import { SINGN_USER } from "../graphql/mutations/singn";
 import { useTheme } from "../theme/themeContext";
 
 const SigninScreen = () => {
+  const router = useRouter();
   const theme = useTheme();
-  const [nom, setNom] = useState("");
-  const [prenom, setPrenom] = useState("");
-  const [mdp, setMdp] = useState("");
-  const [email, setEmail] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    prenom: "",
+    nom: "",
+    email: "",
+    mdp: "",
+  });
+  const [createUser,{}] = useMutation(SINGN_USER, {
+    onCompleted(data){
+      console.log(data, "UserInscription");
+      router.push("/Login")
+    },
+  });
+  const handleSingn = async () => {
+    try {
+      await createUser({ variables: { input: userInfo } });
+    } catch (error) {
+      console.error("Singn error", error);
+    }
+  };
   return (
     <SafeAreaView
       style={[
@@ -26,22 +46,32 @@ const SigninScreen = () => {
       <Title title="Pages & Papilles" />
       <TextAuthPage txt="Inscription" />
       <Input
-        value={prenom}
-        onChangeText={(text) => setPrenom(text)}
+        value={userInfo.prenom}
+        onChangeText={(text) => setUserInfo({...userInfo, prenom: text})}
         label="Prénom"
       />
-      <Input value={nom} onChangeText={(text) => setNom(text)} label="Nom" />
+      <Input value={userInfo.nom} onChangeText={(text) => setUserInfo({...userInfo, nom: text})} label="Nom" />
       <Input
-        value={email}
-        onChangeText={(text) => setEmail(text)}
+        value={userInfo.email}
+        onChangeText={(text) => setUserInfo({...userInfo, email:text})}
         label="Email"
       />
       <Input
-        value={mdp}
-        onChangeText={(text) => setMdp(text)}
+        value={userInfo.mdp}
+        onChangeText={(text)=> setUserInfo({...userInfo, mdp: text})}
         label="Mot de passe"
+        secureTextEntry={!showPassword}
+        iconRight={
+          <Pressable onPress={() => setShowPassword(!showPassword)}>
+            <AntDesign
+              name={showPassword ? "eye" : "eyeo"}
+              size={24}
+              color={theme.colors.primary}
+            />
+          </Pressable>
+        }
       />
-      <Button txt="S'inscrire" />
+      <Button onPress={handleSingn} txt="S'inscrire" />
       <Pressable
         style={[
           styles.PressableTxt,
